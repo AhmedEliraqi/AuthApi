30a204d1ba6a5b622d99ffc8d2c942d9a3874c46
AhmedEliraqi <127881284+AhmedEliraqi@users.noreply.github.com>
2023-08-24 18:18:00 +0300

    first commit

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using AuthApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthApi.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) :base(options)
+        {
+            
+        }
+
+        public DbSet<Users> User { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Users>().ToTable("users");
+        }
+    }
+}
---
+++ Models/UserRole.cs
@@ -0,0 +1,4 @@
+﻿namespace AuthApi.Models
+{
+   
+}
